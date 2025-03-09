e0a9231372bb3eed51d3054e88b693de1e29ee9f
shivaramr <shivaramr.mec@gmail.com>
2025-03-09 13:32:38 +0530

    Update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import DefaultPage from "./pages/DefaultPage";
 import ContentLibrary from "./pages/ContentLibrary";
 import NavBar from "./components/NavBar";
@@ -14,7 +14,7 @@ function App() {
       <div className="content-main">
         <SideBar />
         <Routes>
-          <Route path="/" element={withContentBody(Home)()} />
+          {/* <Route path="/" element={withContentBody(Home)()} /> */}
           <Route path="/*" element={withContentBody(DefaultPage)()} />
           <Route path="/contentLib" element={withContentBody(ContentLibrary)()} />
         </Routes>
